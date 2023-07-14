715b58c9005c02b23fc26dfccb9f5c71ff24c1c7
jasua <75733835+cicero06@users.noreply.github.com>
2023-07-14 09:47:21 +0300

    first commit

---
+++ react-dashboard-last task/src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import "./chartBox.scss";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartBox = (props: Props) => {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>Youth population</span>
+        </div>
+        <h1>1.2 billion young people</h1>
+        <Link to="/" style={{ color: props.color }}>
+          <p>
+According to the World Youth Report (2020), there are 1.2 billion young people aged 15 to 24 years.</p>
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 70 }}
+              />
+
+
+
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
---
+++ react-dashboard-last task/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <span>G20 Countries Comparison</span>
+
+            <span>Dashboard page for report  </span>
+
+        </div>
+    )
+}
+
+export default Footer
